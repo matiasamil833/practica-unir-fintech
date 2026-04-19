c8cfb799b090bf16b2565c5c2bd2301af3d11a6e
emicamsay12 <emicamsay@gmail.com>
2026-04-19 19:38:32 +0000

    add asc/desc order parameter

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sys
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
-
+DEFAULT_ORDER = True
 
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
@@ -24,12 +24,16 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    order = DEFAULT_ORDER
+    if len(sys.argv) >= 3:
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
+        if len(sys.argv) >= 4:
+            order = sys.argv[3].lower() != "desc"
     else:
         print("Se debe indicar el fichero como primer argumento")
         print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("El tercer argumento indica el orden: asc o desc")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
@@ -46,4 +50,4 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    print(sort_list(word_list, ascending=order))
